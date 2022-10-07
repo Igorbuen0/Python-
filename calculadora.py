95bd93446e2e5c3479c00e0fb144c10b07135814
Igorbuen0 <91149974+Igorbuen0@users.noreply.github.com>
2022-10-07 14:44:12 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -8,19 +8,28 @@ while crtl == 0:
     maior = 3
     nova = 4
     sair = 5
+    print('=+=+=+=+=+=+=+=+=+=+=+=+=+=++=+=++=+=++=+=+')
     menu = int(input('\nEscolha sua opção no menu\n[1]somar\n[2]multiplicar\n[3]maior que\n[4]nova opção\n[5]sair\nQual sua opção: '))
-
     if menu == 1:
+        print('=+=+=+=+=+=+=+=+=+=+=+=+=+=++=+=++=+=++=+=+')
         print(f'O resultado é: {num1+num2}')
     if menu == 2:
+        print('=+=+=+=+=+=+=+=+=+=+=+=+=+=++=+=++=+=++=+=+')
         print(f'O resultado é: {num1*num2}')
+
     if menu == 3:
         max_val = max(num1,num2)
+        print('=+=+=+=+=+=+=+=+=+=+=+=+=+=++=+=++=+=++=+=+')
         print(f'O número maior é: {max_val} ')
+
     if menu == 4:
         num1 = int(input('Digite um número: '))
         num2 = int(input('Digite outro número: '))
+        print('=+=+=+=+=+=+=+=+=+=+=+=+=+=++=+=++=+=++=+=+')
     if menu == 5:
         break
+    if menu > 5:
+        print('Erro! Digite as opções informadas!')
+        print('=+=+=+=+=+=+=+=+=+=+=+=+=+=++=+=++=+=++=+=+')
 print('Fim do programa!')
 
